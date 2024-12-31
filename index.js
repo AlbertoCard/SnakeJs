711efebff65c3f521ef6765258151f3cc5c9636e
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-12-30 19:09:52 -0700

    game over

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const $tablero = document.querySelector(".tablero");
 
 let snakeX = 5, snakeY = 12,
     comidaX = 0, comidaY = 0,
-    velocidadX = 0, velocidadY = 0;
+    velocidadX = 0, velocidadY = 0,
+    gameOver = false;
 
 let cuerpo = [];
+
+let intervaloIniciar;
 
 [comidaX, comidaY] = generarComida();
 
@@ -17,25 +20,29 @@ document.addEventListener("keydown", (e) => {
     let snake = document.querySelector(".snake");
     snake.remove();
 
-    if (e.key === "ArrowUp") {
+    if (e.key === "ArrowUp" && velocidadY !== 1) {
         velocidadX = 0;
         velocidadY = -1;
     }
-    if (e.key === "ArrowDown") {
+    if (e.key === "ArrowDown" && velocidadY !== -1) {
         velocidadX = 0;
         velocidadY = 1;
     }
-    if (e.key === "ArrowLeft") {
+    if (e.key === "ArrowLeft" && velocidadX !== 1) {
         velocidadX = -1;
         velocidadY = 0;
     }
-    if (e.key === "ArrowRight") {
+    if (e.key === "ArrowRight" && velocidadX !== -1) {
         velocidadX = 1;
         velocidadY = 0;
     }
 });
 
 const iniciarJuego = () => {
+    if(gameOver){
+        setgameOver();
+    }
+
     if(snakeX === comidaX && snakeY === comidaY){
         [comidaX, comidaY] = generarComida();
         cuerpo.push([snakeX, snakeY]);
@@ -52,12 +59,13 @@ const iniciarJuego = () => {
     snakeX += velocidadX;
     snakeY += velocidadY;
 
-    if(snakeX < 1 || snakeX > 25 || snakeY < 1 || snakeY > 25){
+    if(snakeX < 0 || snakeX > 25 || snakeY < 0 || snakeY > 25){
         snakeX = 5;
         snakeY = 12;
         cuerpo = [];
         velocidadX = 0;
         velocidadY = 0;
+        gameOver = true;
     }
 
     for(let i = 0; i < cuerpo.length; i++){
@@ -68,6 +76,12 @@ const iniciarJuego = () => {
 }
 
 
+const setgameOver = () =>{
+    clearInterval(intervaloIniciar);
+    alert("Game Over");
+    location.reload();
+};
+
 
 
 
@@ -77,6 +91,5 @@ const iniciarJuego = () => {
 
 
 darkTheme(".dark-theme-btn", "dark-theme");
-setInterval(iniciarJuego, 100);
-
+intervaloIniciar = setInterval(iniciarJuego, 100);
 
